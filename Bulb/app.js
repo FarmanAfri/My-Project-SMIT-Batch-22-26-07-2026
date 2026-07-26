d41f46cb5dc38765b65ad8a9c4680af337698e0e
farman afridi <farman.afridi.257005@gmail.com>
2026-07-26 19:13:04 +0500

    All Project

---
+++ Bulb/app.js
@@ -0,0 +1,7 @@
+function bulb(){
+    document.getElementById('img').src = './download__1_-removebg-preview.png'
+}
+
+function bulb2(){
+    document.getElementById('img').src = './images-removebg-preview.png'
+}
---
+++ cal/cal.js
@@ -0,0 +1,18 @@
+function abc(e){
+    document.getElementById('inp').value += e
+}
+
+function add(){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+
+function eq(){
+    document.getElementById('inp').value = ''
+}
+
+function clea(){
+document.getElementById('inp').value = ''
+}
+        
+    
